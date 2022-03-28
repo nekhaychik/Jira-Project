7904cdb0667ee3d95b00ff1acf3454376a46cc61
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-03-28 17:19:57 +0300

    create header, svg-image components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TuiRootModule, TuiDialogModule, TuiNotificationsModule, TUI_SANITIZER } from "@taiga-ui/core";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SvgImageComponent } from './svg-image/svg-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SvgImageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    AppRoutingModule,
+      TuiRootModule,
+      BrowserAnimationsModule,
+      TuiDialogModule,
+      TuiNotificationsModule
+],
+  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
