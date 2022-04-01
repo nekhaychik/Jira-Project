6bcc1f9caa93de3581e0b3f85c686f26ec0f8cf7
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-04-01 19:00:48 +0300

    create enums used in project

---
+++ src/app/enums.ts
@@ -0,0 +1,20 @@
+export enum ButtonAppearance {
+  Primary = 'primary',
+  Secondary = 'secondary'
+}
+
+export enum Icon {
+  IconPlus = 'tuiIconPlus'
+}
+
+export enum Shape {
+  Rounded = 'rounded',
+  Square = 'square',
+  Default = 'none'
+}
+
+export enum Status {
+  Critical = 'critical',
+  Blocked = 'blocked',
+  Normal = 'normal'
+}
