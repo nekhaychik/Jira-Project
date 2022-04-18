a9a6af71cdd376dc41d2aca5a1aea1b54783e846
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-04-18 18:21:05 +0300

    add log out button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { USERS } from 'src/app/mock-users';
+import firebase from "firebase/compat/app";
+import {AuthService} from "../services/auth/auth.service";
+import {Router} from "@angular/router";
+import {ButtonAppearance} from "../enums";
 
 @Component({
   selector: 'app-header',
@@ -10,10 +14,18 @@ export class HeaderComponent implements OnInit {
 
   public logoPath: string = 'assets/logo.svg';
   public userName: string = USERS[0];
+  public user: firebase.User | null = null;
+  public buttonContent: string = 'Log out';
+  public buttonAppearance: ButtonAppearance = ButtonAppearance.Secondary;
 
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.user$.subscribe((value: firebase.User | null) => this.user = value);
+  }
+
+  public logout(): void {
+    this.authService.signOut().subscribe(() => this.router.navigate(['/authorization']));
   }
 
 }
