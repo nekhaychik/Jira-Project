a0acc1854b6c5b17498eee3cfd12beb48c2e438e
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-04-18 18:14:23 +0300

    add authorization guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './services/auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/authorization', pathMatch: 'full' },
-  { path: 'main', component: MainPageComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', canActivate: [AuthGuard], component: MainPageComponent },
   { path: 'authorization', component: AuthComponent }
 ];
 
---
+++ src/app/services/auth/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { map, Observable, take, tap } from 'rxjs';
+import { AuthService } from './auth.service';
+import firebase from 'firebase/compat/app';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.user$.pipe(
+      take(1),
+      map((user: firebase.User | null) => !!user),
+      tap((isLogged: boolean) => {
+        if (!isLogged) {
+          this.router.navigate(['/authorization']);
+        }
+      }),
+    )
+  }
+
+}
