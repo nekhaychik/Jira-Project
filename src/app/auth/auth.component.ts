49dc1af317757fbf1e59069677fc03b7ef46ab08
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-04-26 17:34:11 +0300

    refactor

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import firebase from "firebase/compat/app";
-import {AuthService} from "../services/auth/auth.service";
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import firebase from 'firebase/compat/app';
+import {AuthService} from '../services/auth/auth.service';
+import {CrudService} from '../services/crud/crud.service';
+import {Observable} from 'rxjs';
+import {UserStore} from '../services/types';
+import {Collection} from '../enums';
 
 @Component({
   selector: 'app-auth',
@@ -12,15 +16,37 @@ export class AuthComponent implements OnInit {
 
   public buttonContent: string = 'Log In';
   public user: firebase.User | null = null;
+  public data: UserStore[] = [];
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService, private crudService: CrudService) {
+  }
 
   ngOnInit(): void {
+    this.crudService.handleData<UserStore>(Collection.USERS).subscribe((value: UserStore[]) => {
+      this.data = value;
+    });
     this.authService.user$.subscribe((value: firebase.User | null) => this.user = value);
   }
 
-  public login(): void {
-    this.authService.googleSignIn().subscribe(() => this.authService.user$.subscribe(() => this.router.navigate(['/'])));
+  addUser() {
+    for (let user of this.data) {
+      if (user.uid === this.user?.uid) return;
+      else {
+        this.crudService.createObject(Collection.USERS, {
+          name: this.user?.displayName,
+          uid: this.user?.uid,
+          avatarUrl: this.user?.photoURL,
+        });
+      }
+    }
   }
 
+  public login(): void {
+    this.authService.googleSignIn().subscribe(() => {
+      this.authService.user$.subscribe(() => {
+        this.router.navigate(['/'])
+        this.addUser();
+      });
+    })
+  }
 }
