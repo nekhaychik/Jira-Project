e79a25f37509fc211ca82562e24748d0e1cae39b
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-05-10 11:01:58 +0300

    sort boards for users

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
-import {BoardStore} from '../services/types';
+import {BoardStore, UserStore} from '../services/types';
 import {Collection} from '../enums';
 import {CrudService} from '../services/crud/crud.service';
+import {AuthService} from "../services/auth/auth.service";
+import firebase from "firebase/compat";
 
 @Component({
   selector: 'app-sidebar',
@@ -11,12 +13,44 @@ import {CrudService} from '../services/crud/crud.service';
 })
 export class SidebarComponent implements OnInit {
 
+  public boards: BoardStore[] = [];
+  private userAuth: firebase.User | null = null;
+  public user: UserStore | undefined;
   public boards$: Observable<BoardStore[]> = this.crudService.handleData<BoardStore>(Collection.BOARDS);
+  public users$: Observable<UserStore[]> = this.crudService.handleData<UserStore>(Collection.USERS);
 
-  constructor(private crudService: CrudService) {
+  constructor(private crudService: CrudService,
+              private authService: AuthService) {
   }
 
   ngOnInit(): void {
+    this.authService.user$.subscribe((value: firebase.User | null) => {
+      this.userAuth = value;
+      this.getUser();
+    });
+    this.getBoards();
+  }
+
+  private getUser(): void {
+    this.users$.subscribe((users: UserStore[]) => {
+      let allUsers: UserStore[] = users as UserStore[];
+      allUsers = allUsers.filter((user: UserStore) => user.uid === this.userAuth?.uid);
+      this.user = allUsers[0];
+    });
+  }
+
+  private getBoards(): void {
+    this.boards$.subscribe((boards: BoardStore[]) => {
+      this.boards = [];
+      boards.forEach(board => {
+        board.membersID.forEach(memberID => {
+          if (memberID === this.user?.id){
+            this.boards.push(board);
+            return;
+          }
+        });
+      });
+    });
   }
 
   public trackByFn(index: number, item: BoardStore): number {
