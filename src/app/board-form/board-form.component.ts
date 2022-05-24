fe02c1101e5670d35c57fb22a22a2ff1eb191c40
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-05-24 17:58:37 +0300

    fix board form

--- src/app/board-form/board-form.component.ts
+++ src/app/board-form/board-form.component.ts
@@ -6,6 +6,8 @@ import {Board, UserStore} from '../services/types';
 import {Collection} from '../enums';
 import {Observable} from 'rxjs';
 import {Router} from "@angular/router";
+import firebase from "firebase/compat";
+import {AuthService} from "../services/auth/auth.service";
 
 @Component({
   selector: 'app-board-form',
@@ -23,6 +25,7 @@ export class BoardFormComponent implements OnInit {
   isCreating: boolean = true;
   @Input()
   boardName: string = '';
+  private authUser: firebase.User | null = null;
 
   public boardForm: FormGroup = new FormGroup({});
   public formControls: typeof BoardControls = BoardControls;
@@ -30,11 +33,14 @@ export class BoardFormComponent implements OnInit {
   public users$: Observable<UserStore[]> = this.crudService.handleData<UserStore>(Collection.USERS);
 
   constructor(private router: Router,
-              private crudService: CrudService) {
+              private crudService: CrudService,
+              private authService: AuthService,) {
   }
 
   public ngOnInit(): void {
-    console.log(this.isCreating, this.board)
+    this.authService.user$.subscribe((value: firebase.User | null) => {
+      this.authUser = value;
+    });
     this.boardForm.addControl(BoardControls.name, new FormControl(this.boardName, Validators.compose([Validators.required, Validators.maxLength(16)])));
     this.boardForm.addControl(BoardControls.membersID, new FormControl('', Validators.required));
   }
@@ -55,6 +61,12 @@ export class BoardFormComponent implements OnInit {
         name: this.boardForm?.controls[BoardControls.name].value,
         membersID: this.boardForm.controls[BoardControls.membersID].value
       };
+      if (this.authUser) {
+        if (!board.membersID.includes(this.authUser.uid)) {
+          board.membersID.push(this.authUser.uid);
+        }
+      }
+
       this.addBoard(board);
       this.boardForm?.reset();
     } else {
