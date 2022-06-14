2242a232e2607f96c9d8c0620f7eeef51c69a003
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-06-14 13:20:40 +0300

    create filter pipe

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,10 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+  transform(arrayOfValues: any, searchValue: string) {
+    return !searchValue ? arrayOfValues: arrayOfValues.filter((value: any) => value.name.toString().toLowerCase().includes(searchValue.toLowerCase()));
+  }
+}
