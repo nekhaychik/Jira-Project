78051d00223081b471ea6f21d5a0e3237185ed79
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-06-14 13:21:08 +0300

    create form validator

---
+++ src/app/validators.ts
@@ -0,0 +1,10 @@
+import {AbstractControl, ValidatorFn} from "@angular/forms";
+
+export function nameExistValidator(existingNames: string[]): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: boolean } | null => {
+    if(control.value !== undefined && existingNames.includes(control.value.trim())) {
+      return { 'exist': true };
+    }
+    return null;
+  };
+}
